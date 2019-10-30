0074e4d8a3a425540520c65e25cdec7330c8fa72
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2019-10-30 20:30:11 +0800

    Added router guards to prevent accessing of pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { TokenService } from '@/services/storage.service'
 
 Vue.use(VueRouter)
 
@@ -7,7 +8,11 @@ const routes = [
   {
     path: '/',
     name: 'login',
-    component: () => import('../views/Login')
+    component: () => import('../views/Login'),
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: true
+    }
   },
   {
     path: '/search',
@@ -20,6 +25,30 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some(record => record.meta.public)
+  const onlyWhenLoggedOut = to.matched.some(
+    record => record.meta.onlyWhenLoggedOut
+  )
+  const loggedIn = !!TokenService.getToken()
+
+  // if route is private & user not loggedIn
+  // redirect to login page
+  if (!isPublic && !loggedIn) {
+    return next({
+      path: '/',
+      query: { redirect: to.fullPath } // Store the full path to redirect the user to after login
+    })
+  }
+
+  // Do not allow user to visit login page if they are logged in
+  if (loggedIn && onlyWhenLoggedOut) {
+    return next('/search')
+  }
+
+  next()
 })
 
 export default router
